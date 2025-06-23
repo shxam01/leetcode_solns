754a8fc0722bc68b6d40460fbbe37cbd98fceab1
SHYAM MISHRA <shxamishra@gmail.com>
2025-06-23 05:57:46 +0530

    Time: 11 ms (49.57%) | Memory: 96.6 MB (55.98%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    int n,m;
+
+    bool valid(int x,int y){
+        return x>=0 && x<n && y>=0 && y<m;
+    }
+
+    int dx[4]={-1,1,0,0};
+    int dy[4]={0,0,-1,1};
+
+    int bfs(vector<vector<int>>&grid,int x_src,int y_src,vector<vector<bool>>&vis){
+
+        int ans=0;
+
+        vis[x_src][y_src]=true;
+
+        queue<pair<int,pair<int,int>>>q;
+
+        int curr=grid[x_src][y_src];
+
+        q.push({curr,{x_src,y_src}});
+
+        while(!q.empty()){
+            auto &pr=q.front();
+
+            auto curr_fish=pr.first;
+
+            auto coordinates=pr.second;
+
+            auto x=coordinates.first;
+
+            auto y=coordinates.second;
+
+            q.pop();
+
+            ans=max(ans,curr_fish);
+
+
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+
+                if(valid(nx,ny) && !vis[nx][ny] && grid[nx][ny]>0){
+                    vis[nx][ny]=true;
+                    int new_fish_count=curr_fish+grid[nx][ny];
+
+                    q.push({new_fish_count,{nx,ny}});
+                }
+            }
+        }
+        return ans;
+    }
+
+    int dfs(vector<vector<int>>&grid,int x_src,int y_src,vector<vector<bool>>&vis){
+        vis[x_src][y_src]=true;
+
+        int ans=grid[x_src][y_src];
+
+        for(int i=0;i<4;i++){
+            int nx=x_src+dx[i];
+            int ny=y_src+dy[i];
+
+            if(valid(nx,ny) && !vis[nx][ny] && grid[nx][ny]>0){
+                ans +=dfs(grid,nx,ny,vis);
+            }
+        }
+        return ans;
+    }
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]>0){
+                    ans=max(ans,dfs(grid,i,j,vis));
+                }
+            }
+        }
+        return ans;
+    }
+};
