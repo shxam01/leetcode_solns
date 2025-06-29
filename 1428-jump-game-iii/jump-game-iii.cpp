c637fbdd5c047b20df54948d03aff1926af30edb
SHYAM MISHRA <shxamishra@gmail.com>
2025-06-29 05:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (85.77%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int n;
+    // bool helper(vector<int>&arr,int idx,vector<bool>&vis){
+
+    //     if(idx<0 || idx>=n) return false;
+
+    //     if(vis[idx]) return false;
+
+    //     if(arr[idx]==0) return true;
+
+    //     vis[idx]=true;
+
+    //     bool res=false;
+        
+
+    //     int jump_length=arr[idx];
+
+        
+
+    //     if(idx-jump_length>=0){
+    //         res |=helper(arr,idx-jump_length,vis);
+    //     }
+
+    //     if(idx+jump_length < n) {
+    //         res |=helper(arr,idx+jump_length,vis);
+    //     }
+
+    //     vis[idx]=false;
+
+    //     return res;
+
+
+    // } 
+    bool canReach(vector<int>& arr, int start) {
+        // unordered_map<int,vector<int>>adj;
+
+        // adj[start].push_back()
+
+        n=arr.size();
+
+        vector<bool>vis(n,false);
+
+        // return helper(arr,start,vis);
+
+        queue<int>q;
+
+        q.push(start);
+
+        while(!q.empty()){
+            auto idx=q.front();
+
+            q.pop();
+
+            if(arr[idx]==0) return true;
+
+            int jump_length=arr[idx];
+
+
+            if(idx-jump_length>=0 && !vis[idx-jump_length]){
+                vis[idx-jump_length]=true;
+                q.push(idx-jump_length);
+            }
+
+            if(idx+jump_length<n && !vis[idx+jump_length]){
+                vis[idx+jump_length]=true;
+                q.push(idx+jump_length);
+            }
+        }
+        return false;
+        
+    }
+};
