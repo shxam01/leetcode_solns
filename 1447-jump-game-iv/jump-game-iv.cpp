9ec7b4987b914cc6b331fdba8064764b0eba34d5
SHYAM MISHRA <shxamishra@gmail.com>
2025-06-29 06:44:57 +0530

    Time: 92 ms (59.83%) | Memory: 80.4 MB (50.64%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.cpp
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -1,50 +1,57 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> mp;
+    unordered_map<int,vector<int>>mp;
 
+    int dp[50001];
+    
     int minJumps(vector<int>& arr) {
-        int n = arr.size();
-        vector<bool> vis(n, false);
+        int n=arr.size();
+        vector<bool>vis(n,false);
 
-        for(int i = 0; i < n; i++) {
+        memset(dp,-1,sizeof(dp));
+
+        for(int i=0;i<n;i++){
             mp[arr[i]].push_back(i);
         }
 
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        vis[0] = true;
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vis[0]=true;
 
-        while(!q.empty()) {
-            auto [curr_idx, moves] = q.front();
+        while(!q.empty()){
+            auto [curr_idx,moves]=q.front();
+
             q.pop();
 
-            if(curr_idx == n - 1) {
+            // if(dp[curr_idx]!=-1) return dp[curr_idx];
+
+            if(curr_idx==n-1){
                 return moves;
             }
-
-            // Same value indices
-            int curr_val = arr[curr_idx];
-            for(auto &next_idx : mp[curr_val]) {
-                if(!vis[next_idx]) {
-                    vis[next_idx] = true;
-                    q.push({next_idx, moves + 1});
+            int curr_value=arr[curr_idx];
+            for(auto &next_val:mp[curr_value]){
+                if(!vis[next_val]){
+                    q.push({next_val,moves+1});
                 }
             }
-            mp[curr_val].clear(); // Clear to avoid reprocessing
+            mp[curr_value].clear();
 
-            // Previous index
-            if(curr_idx - 1 >= 0 && !vis[curr_idx - 1]) {
-                vis[curr_idx - 1] = true;
-                q.push({curr_idx - 1, moves + 1});
+            if(curr_idx-1>=0 && !vis[curr_idx-1]){
+                vis[curr_idx-1]=true;
+                q.push({curr_idx-1,moves+1});
             }
 
-            // Next index
-            if(curr_idx + 1 < n && !vis[curr_idx + 1]) {
-                vis[curr_idx + 1] = true;
-                q.push({curr_idx + 1, moves + 1});
+            if(curr_idx+1<n && !vis[curr_idx+1]){
+                vis[curr_idx+1]=true;
+                q.push({curr_idx+1,moves+1});
             }
+
+            
         }
 
         return -1;
+
+
+        
     }
 };
