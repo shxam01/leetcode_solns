45e695af0da9d50f9789be0ebf9f66bc934c4697
SHYAM MISHRA <shxamishra@gmail.com>
2025-10-05 22:40:45 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (5.98%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>leftsum(n);
+        vector<int>rightsum(n);
+        leftsum[0]=0;
+        for(int i=1;i<n;i++){
+            leftsum[i]=leftsum[i-1]+nums[i-1];
+        }
+
+        rightsum[n-1]=0;
+
+        for(int i=n-2;i>=0;i--){
+            rightsum[i]=rightsum[i+1]+nums[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            if(leftsum[i]==rightsum[i]) return i;
+        }
+        return -1;
+    }
+};
