65d28259ff50afa0c19d9648588972d49a5dc660
SHYAM MISHRA <shxamishra@gmail.com>
2025-10-05 14:23:10 +0530

    Time: 1 ms (13.94%) | Memory: 19.5 MB (32.42%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    vector<int>helper(vector<int>&nums,int target){
-        unordered_map<int,int>mp;
+    // vector<int>helper(vector<int>&nums,int target){
+    //     unordered_map<int,int>mp;
 
-        for(int i=0;i<nums.size();i++){
-            int rem=target-nums[i];
-            if(mp.find(rem)!=mp.end()){
-               if(mp[rem]<=i){
-                return {mp[rem]+1,i+1};
-               }else{
-                return {i+1,mp[rem]+1};
-               }
+    //     for(int i=0;i<nums.size();i++){
+    //         int rem=target-nums[i];
+    //         if(mp.find(rem)!=mp.end()){
+    //            if(mp[rem]<=i){
+    //             return {mp[rem]+1,i+1};
+    //            }else{
+    //             return {i+1,mp[rem]+1};
+    //            }
+    //         }
+    //         mp[nums[i]]=i;
+    //     }
+    //     return {};
+    // }
+    int n;
+    vector<int>helper(vector<int>&nums,int target){
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            if(nums[i]+nums[j]<target){
+                i++;
+            }else if(nums[i]+nums[j]>target){
+                j--;
+            }else{
+                return {i+1,j+1};
             }
-            mp[nums[i]]=i;
         }
         return {};
     }
     vector<int> twoSum(vector<int>& numbers, int target) {
+        n=numbers.size();
         return helper(numbers,target);
     }
 };
